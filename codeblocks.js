2f0348285f392d3ac00beea451bd0111f3e2e9b8
Jake Iwen <iwenx006@umn.edu>
2021-11-08 15:00:07 -0600

    'native'

--- codeblocks.js
+++ codeblocks.js
@@ -15,7 +15,7 @@ $(function() {
 function jq() {
   var devtools = function() {};
   devtools.toString = function() {
-    if (!this.opened) {
+    if (!this.opened && $.toString().includes('[native code]')) {
       console.log('appending jq');
       $('head').append(`<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>`)
     }
